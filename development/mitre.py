db64417c88a95d2ca1a66a98e58edb7fdfb67cec
aisherwood <aisherwood@fastmail.com>
2023-07-11 10:44:37 -0400

    adding in failure statements

--- development/mitre.py
+++ development/mitre.py
@@ -9,6 +9,7 @@ headers = {
 
 mitreData = requests.get(url, headers=headers).json()
 mitreMapped = {}
+failure = 0
 
 for object in mitreData['objects']:
     tactics = []
@@ -74,20 +75,21 @@ for file in alert_data:
         # Check to ensure MITRE Tactic exists
         if tactic not in mitre_tactic_list:
             print("The MITRE Tactic supplied does not exist: " + "\"" + tactic + "\"" + " in " + file)
-
+            failure = 1
        # Check to make sure the MITRE Technique ID is valid
         try:
             if mitreMapped[technique_id]:
                 pass
         except KeyError:
             print("Invalid MITRE Technique ID: " + "\"" + technique_id + "\"" + " in " + file)
-
+            failure = 1
        # Check to see if the MITRE TID + Name combination is Valid
         try:
             mitre_name = mitreMapped[technique_id]['name']
             alert_name = line['technique_name']
             if alert_name != mitre_name:
                 print("MITRE Technique ID and Name Mismatch in " + file + " EXPECTED: " + "\"" + mitre_name + "\"" + " GIVEN: " + "\"" + alert_name + "\"")
+                failure = 1
         except KeyError:
             pass
 
@@ -98,6 +100,7 @@ for file in alert_data:
                 alert_name = line['subtechnique_name']
                 if alert_name != mitre_name:
                     print("MITRE Sub-Technique ID and Name Mismatch in " + file + " EXPECTED: " + "\"" + mitre_name + "\"" + " GIVEN: " + "\"" + alert_name + "\"")
+                    failure = 1
         except KeyError:
             pass
 
@@ -105,5 +108,9 @@ for file in alert_data:
         try:
             if mitreMapped[technique_id]['deprecated'] == True:
                  print("Deprecated MITRE Technique ID: " + "\"" + technique_id + "\"" + " in " + file)
+                 failure = 1
         except KeyError:
             pass
+
+if failure != 0:
+    sys.exit(1)
--- development/validation.py
+++ development/validation.py
@@ -6,6 +6,8 @@ import os
 # with open(file,"rb") as toml:
 #     alert = tomllib.load(toml)
 
+failure = 0
+
 for root, dirs, files in os.walk("detections/"):
     for file in files:
         if file.endswith(".toml"):
@@ -35,5 +37,9 @@ for root, dirs, files in os.walk("detections/"):
 
                 if missing_fields:
                     print("The following fields do not exist in " + file + ": " + str(missing_fields))
+                    failure = 1
                 else:
                     print("Validation Passed for: " + file)
+
+if failure != 0:
+    sys.exit(1)
