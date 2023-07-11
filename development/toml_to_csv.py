08fe5133cf89ee15f0eca6f79a2b530a8a3dc740
aisherwood <aisherwood@fastmail.com>
2023-07-11 17:58:31 -0400

    adding more scripts

---
+++ development/toml_to_csv.py
@@ -0,0 +1,69 @@
+import tomllib
+import os
+
+list = {}
+
+for root, dirs, files in os.walk("detections/"):
+    for file in files:
+        if file.endswith(".toml"):
+            full_path = os.path.join(root, file)
+            with open(full_path,"rb") as toml:
+                alert = tomllib.load(toml)
+                date = alert['metadata']['creation_date']
+                name = alert['rule']['name']
+                author = alert['rule']['author']
+                risk_score = alert['rule']['risk_score']
+                severity = alert['rule']['severity']
+                filtered_object_array = []
+
+                if alert['rule']['threat'][0]['framework'] == "MITRE ATT&CK":
+                    for threat in alert['rule']['threat']:
+                        technique_id = threat['technique'][0]['id']
+                        technique_name = threat['technique'][0]['name']
+
+                        if 'tactic' in threat:
+                            tactic = threat['tactic']['name']
+                        else:
+                            tactic = "none"
+
+                        if 'subtechnique' in threat['technique'][0]:
+                            subtechnique_id = threat['technique'][0]['subtechnique'][0]['id']
+                            subtechnique_name = threat['technique'][0]['subtechnique'][0]['name']
+                        else:
+                            subtechnique_id = "none"
+                            subtechnique_name = "none"
+
+                        technique = technique_id + " - " + technique_name
+                        subtech = subtechnique_id + " - " + subtechnique_name
+
+                        obj = {'tactic': tactic, 'technique': technique, 'subtech': subtech, 'subtech': subtech}
+                        filtered_object_array.append(obj)
+                obj = {'name': name, 'date': date, 'author': author, 'risk_score': risk_score, 'severity': severity, 'mitre': filtered_object_array}
+                list[file] = obj
+
+output_path = "\metrics\detectiondata.csv"
+
+outF = open(output_path, "w")
+outF.write("name,date,author,risk_score,severity,tactic,technique,subtechnique\n")
+
+separator = "; "
+for line in list.values():
+    date = line['date']
+    name = line['name']
+    author = str(line['author']).replace(",",";")
+    risk_score = str(line['risk_score'])
+    severity = line['severity']
+    
+    tactic = []
+    tech = []
+    subtech = []
+
+    for technique in line['mitre']:
+        tactic.append(technique['tactic'])
+        tech.append(technique['technique'])
+        subtech.append(technique['subtech'])
+    outF.write(name + "," + date + "," + author + "," + risk_score + "," + severity + "," + separator.join(tactic) + "," + separator.join(tech) + "," + separator.join(subtech) + "\n")
+outF.close()
+
+
+                
---
+++ development/toml_to_navigator.py
@@ -0,0 +1,123 @@
+import tomllib
+import os
+
+output_path = "I:\Google Drive\TCM\Python\metrics\\navigator.json"
+techniques = {}
+
+for root, dirs, files in os.walk("detections/"):
+    for file in files:
+        if file.endswith(".toml"):
+            full_path = os.path.join(root, file)
+            with open(full_path,"rb") as toml:
+                alert = tomllib.load(toml)
+
+                for threat in alert['rule']['threat']:
+                    technique_id = str(threat['technique'][0]['id'])
+                    tactic = str(threat['tactic']['name'])
+
+                    if technique_id not in techniques:
+                        obj = {'technique_id': technique_id, 'tactic': tactic, 'count': 1}
+                        techniques[technique_id] = obj
+                    else:
+                        techniques[technique_id]['count'] += 1
+                    
+                    if 'subtechnique' in threat['technique'][0]:
+                        subtechnique = threat['technique'][0]['subtechnique'][0]['id']
+                        if subtechnique not in techniques:
+                            obj = {'technique_id': subtechnique, 'tactic': tactic, 'count': 1}
+                            techniques[subtechnique] = obj
+                        else:
+                            techniques[subtechnique]['count'] += 1
+
+
+beginning = """{
+	"name": "Custom Detections",
+	"versions": {
+		"attack": "13",
+		"navigator": "4.8.2",
+		"layer": "4.4"
+	},
+	"domain": "enterprise-attack",
+	"description": "",
+	"filters": {
+		"platforms": [
+			"Linux",
+			"macOS",
+			"Windows",
+			"Network",
+			"PRE",
+			"Containers",
+			"Office 365",
+			"SaaS",
+			"Google Workspace",
+			"IaaS",
+			"Azure AD"
+		]
+	},
+	"sorting": 0,
+	"layout": {
+		"layout": "side",
+		"aggregateFunction": "average",
+		"showID": false,
+		"showName": true,
+		"showAggregateScores": false,
+		"countUnscored": false
+	},
+	"hideDisabled": false,
+    """
+
+end = """
+    ],
+	"gradient": {
+		"colors": [
+			"#ff6666ff",
+			"#ffe766ff",
+			"#8ec843ff"
+		],
+		"minValue": 0,
+		"maxValue": 3
+	},
+	"legendItems": [],
+	"metadata": [],
+	"links": [],
+	"showTacticRowBackground": false,
+	"tacticRowBackground": "#dddddd",
+	"selectTechniquesAcrossTactics": true,
+	"selectSubtechniquesWithParent": false
+}"""
+
+counter = 0
+total_techniques = 0
+
+for technique in techniques:
+    total_techniques += 1
+
+output_path = "metrics\\navigator.json"
+
+outF = open(output_path, "w")
+outF.write(beginning)
+outF.write("\"techniques\": [")
+
+for key in techniques:
+    counter += 1
+    technique_id = techniques[key]['technique_id']
+    count = str(techniques[key]['count'])
+    tactic = techniques[key]['tactic'].lower()
+    outF.write("\n\t\t{")
+    outF.write("\n\t\t\t\"techniqueID\": \"" + technique_id + "\",")
+    outF.write("\n\t\t\t\"tactic\": \"" + tactic + "\",")
+    outF.write("\n\t\t\t\"score\": " + count + ",")
+    outF.write("\n\t\t\t\"color\": \"\""",")
+    outF.write("\n\t\t\t\"comment\": \"\""",")
+    outF.write("\n\t\t\t\"enabled\": true,")
+    outF.write("\n\t\t\t\"metadata\": [],")
+    outF.write("\n\t\t\t\"links\": [],")
+    outF.write("\n\t\t\t\"showSubtechniques\": false")
+
+    if counter != total_techniques:
+        outF.write("\n\t\t},")
+    else:
+        outF.write("\n\t\t}")
+
+outF.write(end)
+outF.close()
