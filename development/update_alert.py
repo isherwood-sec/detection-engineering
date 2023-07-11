12f321a9bf28adca5de01e29d8132e587c8430f9
aisherwood <aisherwood@fastmail.com>
2023-07-11 16:48:06 -0400

    adding files

--- development/update_alert.py
+++ development/update_alert.py
@@ -50,14 +50,14 @@ for root, dirs, files in os.walk("detections/"):
 
                 data += "  \"enabled\": true\n}"
             
-                rule_id = alert['rule']['rule_id']
-                update_url = url + "?rule_id=" + rule_id
+            rule_id = alert['rule']['rule_id']
+            url = url + "?rule_id=" + rule_id
+        
+            elastic_data = requests.put(url, headers=headers, data=data).json()
+        
+            for key in elastic_data:
+                if key == "status_code":
+                    if 404 == elastic_data["status_code"]:
+                        elastic_data = requests.post(url, headers=headers, data=data).json()
+                        print(elastic_data)
 
-                elastic_data = requests.put(update_url, headers=headers, data=data).json()
-                print(elastic_data)
-
-                for key in elastic_data:
-                    if key == "status_code":
-                        if 404 == elastic_data["status_code"]:
-                            elastic_data = requests.post(url, headers=headers, data=data).json()
-                            print(elastic_data)
