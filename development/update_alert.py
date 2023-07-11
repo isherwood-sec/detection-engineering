cd81c573a35266f13eec222ab619f7f7d9d0ca0b
aisherwood <aisherwood@fastmail.com>
2023-07-11 13:24:18 -0400

    test

--- development/update_alert.py
+++ development/update_alert.py
@@ -1,56 +1,62 @@
-import requests
-import os
-import tomllib
+import json
+data = json.loads(".github/outputs/added_files.json")
+print(data)
 
-url = "https://detectionengineering101.kb.us-central1.gcp.cloud.es.io:9243/api/detection_engine/rules"
-api_key = os.environ['ELASTIC_KEY']
 
-headers = {
-    'Content-Type': 'application/json;charset=UTF-8',
-    'kbn-xsrf': 'true',
-    'Authorization': 'ApiKey ' + api_key
-}
 
-data = ""
+# import requests
+# import os
+# import tomllib
 
-for root, dirs, files in os.walk("detections/"):
-    for file in files:
-        data = "{\n"
-        if file.endswith(".toml"):
-            full_path = os.path.join(root, file)
-            with open(full_path,"rb") as toml:
-                alert = tomllib.load(toml)
+# url = "https://detectionengineering101.kb.us-central1.gcp.cloud.es.io:9243/api/detection_engine/rules"
+# api_key = os.environ['ELASTIC_KEY']
 
-            if alert['rule']['type'] == "query": # query based alert
-                required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','threat']
-            elif alert['rule']['type'] == "eql": # event correlation alert
-                required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','language','threat']
-            elif alert['rule']['type'] == "threshold": # threshold based alert
-                required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','threshold','threat']
-            else:
-                print("Unsupported rule type found in: " + full_path)
-                break
+# headers = {
+#     'Content-Type': 'application/json;charset=UTF-8',
+#     'kbn-xsrf': 'true',
+#     'Authorization': 'ApiKey ' + api_key
+# }
+
+# data = ""
+
+# for root, dirs, files in os.walk("detections/"):
+#     for file in files:
+#         data = "{\n"
+#         if file.endswith(".toml"):
+#             full_path = os.path.join(root, file)
+#             with open(full_path,"rb") as toml:
+#                 alert = tomllib.load(toml)
+
+#             if alert['rule']['type'] == "query": # query based alert
+#                 required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','threat']
+#             elif alert['rule']['type'] == "eql": # event correlation alert
+#                 required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','language','threat']
+#             elif alert['rule']['type'] == "threshold": # threshold based alert
+#                 required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','threshold','threat']
+#             else:
+#                 print("Unsupported rule type found in: " + full_path)
+#                 break
             
-            for field in alert['rule']:
-                if field in required_fields:
-                    if type(alert['rule'][field]) == list:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
-                    elif type(alert['rule'][field]) == str:
-                        if field == 'description':
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"").replace("\\","\\\\") + "\"," + "\n"
-                        elif field == 'query':
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"","\\\"").replace("\n"," ") + "\"," + "\n"
-                        else:
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"") + "\"," + "\n"
-                    elif type(alert['rule'][field]) == int:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]) + "," + "\n"
-                    elif type(alert['rule'][field]) == dict:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
+#             for field in alert['rule']:
+#                 if field in required_fields:
+#                     if type(alert['rule'][field]) == list:
+#                         data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
+#                     elif type(alert['rule'][field]) == str:
+#                         if field == 'description':
+#                             data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"").replace("\\","\\\\") + "\"," + "\n"
+#                         elif field == 'query':
+#                             data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"","\\\"").replace("\n"," ") + "\"," + "\n"
+#                         else:
+#                             data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"") + "\"," + "\n"
+#                     elif type(alert['rule'][field]) == int:
+#                         data += "  " + "\"" + field + "\": " + str(alert['rule'][field]) + "," + "\n"
+#                     elif type(alert['rule'][field]) == dict:
+#                         data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
                     
-            data += "  \"enabled\": true\n}"
+#             data += "  \"enabled\": true\n}"
 
-        rule_id = alert['rule']['rule_id']
-        url = url + "?rule_id=" + rule_id
+#         rule_id = alert['rule']['rule_id']
+#         url = url + "?rule_id=" + rule_id
         
-        elastic_data = requests.put(url, headers=headers, data=data).json()
-        print(elastic_data)
+#         elastic_data = requests.put(url, headers=headers, data=data).json()
+#         print(elastic_data)
